77fd7c6a15b0e870df4cc22ce53e822222b6d45f
MitchellDan <mitchelldan@urbandalescools.com>
2020-09-21 15:42:54 -0500

    Did Ch3 Trapezoid

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,10 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+base1 = input("What is the first base(width) dimension? ")
+base2 = input("What is the second base(width) dimension? ")
+height = input("What is the height dimension? ")
+
+area = ((int(base1) + int(base2)) / 2) * int(height)
+
+print ("The area is " + str(area))
